fc693f543ec71c639268705fdca8c41e2205bcb8
omorovm <omorovmeder23@gmail.com>
2024-02-04 17:19:16 +0600

    user \& settings

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     # apps
     'user',
     'comment',
@@ -101,7 +101,7 @@ DATABASES = {
     }
 }
 
-AUTH_USER_MODEL = 'account.CustomUser'
+AUTH_USER_MODEL = 'user.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- user/models.py
+++ user/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **kwargs):
+        if not email:
+            return ValueError('почта должна обяз передаваться')
+        email = self.normalize_email(email=email)
+        user = self.model(email=email, **kwargs)
+        user.create_activation_code()
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_user(self, email, password, **kwargs):
+        kwargs.setdefault('is_staff', False)
+        kwargs.setdefault('is_superuser', False)
+        return self._create_user(email, password, **kwargs)
+
+    def create_superuser(self, email, password, **kwargs):
+        kwargs.setdefault('is_staff', True)
+        kwargs.setdefault('is_superuser', True)
+        kwargs.setdefault('is_active', True)
+        return self._create_user(email, password, **kwargs)
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    activation_code = models.CharField(max_length=255, blank=True)
+    username = models.CharField(max_length=100, blank=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def create_activation_code(self):
+        import uuid
+        code = str(uuid.uuid4())
+        self.activation_code = code
+    
+    def __str__(self) -> str:
+        return self.email
